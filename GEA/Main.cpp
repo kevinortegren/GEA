489d150dd931781983b840d5b078ebd8f06c39bf
Daniel Carlsson <dc.jarle.g@gmail.com>
2014-09-10 11:52:31 +0200

    Removes Simple pool test.

--- GEA/Main.cpp
+++ GEA/Main.cpp
@@ -22,9 +22,6 @@ const size_t POOL_TEST_THREADED_PARTICLE_COUNT = 4096;
 const size_t POOL_TEST_THREADED_PARTICLE_MAX_LIFETIME = 8;
 const size_t POOL_TEST_THREADED_WORKER_COUNT = 4;
 
-const size_t SIMPLE_POOL_TEST_FRAME_COUNT = 1024;
-const size_t SIMPLE_POOL_PARTICLE_COUNT = 1024;
-
 struct Particle
 {
 	Particle(int framesLeftToLive) 
@@ -39,9 +36,6 @@ struct Particle
 int RND[POOL_TEST_PARTICLE_COUNT];
 int RNDThreaded[POOL_TEST_THREADED_PARTICLE_COUNT];
 int RNDStack[STACK_TEST_OBJECTS_PER_WORKER];
-
-template <typename T>
-void SimplePoolTestUnthreaded(T& allocator, std::fstream& p_file);
 
 template <typename T>
 void PoolTestUnthreaded(T& allocator, std::fstream& p_file);
@@ -85,7 +79,6 @@ int main()
 		rndStackSum += RNDStack[i];
 	}
 
-	/*
 	//Print stack test parameters
 	ColorCMD::SetTextColor(ColorCMD::ConsoleColor::AQUA);
 	std::cout << "STACK_TEST_WORKER_COUNT: " << STACK_TEST_WORKER_COUNT << std::endl;
@@ -106,20 +99,17 @@ int main()
 	std::cout << "-- Stack Test Threaded (Default) --" << std::endl;  double stackTestDefaultThreadedAvg = StackTestDefault();	std::cout << std::endl;
 
 	std::cout << "Average Frame Time Difference: " << abs(stackTestCustomThreadedAvg - stackTestDefaultThreadedAvg) << std::endl << std::endl;
-	*/
 
 	DefaultMemoryManager defaultMM(sizeof(Particle));
 	PoolAllocator poolMM(sizeof(Particle), POOL_TEST_PARTICLE_COUNT);
 	ThreadedPoolAllocator threadedPoolMM(sizeof(Particle), POOL_TEST_THREADED_PARTICLE_COUNT * POOL_TEST_THREADED_WORKER_COUNT);
 
-	std::fstream file[7];
-	file[0].open("pool_simple_custom.csv", std::ios_base::trunc | std::ios_base::out);
-	file[1].open("pool_unthreaded_custom.csv", std::ios_base::trunc | std::ios_base::out);
-	file[2].open("pool_threaded_custom.csv", std::ios_base::trunc | std::ios_base::out);
-	file[3].open("pool_simple_default.csv", std::ios_base::trunc | std::ios_base::out);
-	file[4].open("pool_unthreaded_default.csv", std::ios_base::trunc | std::ios_base::out);
-	file[5].open("pool_threaded_default.csv", std::ios_base::trunc | std::ios_base::out);
-	file[6].open("multiple_pool_threaded_custom.csv", std::ios_base::trunc | std::ios_base::out);
+	std::fstream file[5];
+	file[0].open("pool_unthreaded_custom.csv", std::ios_base::trunc | std::ios_base::out);
+	file[1].open("pool_threaded_custom.csv", std::ios_base::trunc | std::ios_base::out);
+	file[2].open("pool_unthreaded_default.csv", std::ios_base::trunc | std::ios_base::out);
+	file[3].open("pool_threaded_default.csv", std::ios_base::trunc | std::ios_base::out);
+	file[4].open("multiple_pool_threaded_custom.csv", std::ios_base::trunc | std::ios_base::out);
 
 	//Print pool test parameters
 	ColorCMD::SetTextColor(ColorCMD::ConsoleColor::AQUA);
@@ -129,8 +119,8 @@ int main()
 	std::cout << std::endl;
 	ColorCMD::SetTextColor(ColorCMD::ConsoleColor::WHITE);
 
-	std::cout << "-- Pool Test Unthreaded (Custom) --" << std::endl;				PoolTestUnthreaded(poolMM, file[1]);				std::cout << std::endl;
-	std::cout << "-- Pool Test Unthreaded (Default) --" << std::endl;				PoolTestUnthreaded(defaultMM, file[4]);				std::cout << std::endl;
+	std::cout << "-- Pool Test Unthreaded (Custom) --" << std::endl;				PoolTestUnthreaded(poolMM, file[0]);				std::cout << std::endl;
+	std::cout << "-- Pool Test Unthreaded (Default) --" << std::endl;				PoolTestUnthreaded(defaultMM, file[2]);				std::cout << std::endl;
 	std::cout << std::endl;
 
 	ColorCMD::SetTextColor(ColorCMD::ConsoleColor::AQUA);
@@ -141,53 +131,14 @@ int main()
 	std::cout << std::endl;
 	ColorCMD::SetTextColor(ColorCMD::ConsoleColor::WHITE);
 
-	std::cout << "-- Pool Test Threaded (Custom) --" << std::endl;					PoolTestThreaded(threadedPoolMM, file[2]);			std::cout << std::endl;
-	std::cout << "-- Pool Test Threaded (Default) --" << std::endl;					PoolTestThreaded(defaultMM, file[5]);				std::cout << std::endl;
+	std::cout << "-- Pool Test Threaded (Custom) --" << std::endl;					PoolTestThreaded(threadedPoolMM, file[1]);			std::cout << std::endl;
+	std::cout << "-- Pool Test Threaded (Default) --" << std::endl;					PoolTestThreaded(defaultMM, file[3]);				std::cout << std::endl;
 
-	std::cout << "-- Multiple Pool Test Threaded (Custom) --" << std::endl;			MultiplePoolTestThreaded(file[6]);				std::cout << std::endl;
+	std::cout << "-- Multiple Pool Test Threaded (Custom) --" << std::endl;			MultiplePoolTestThreaded(file[4]);				std::cout << std::endl;
 
 	std::cout << "Hej" << std::endl;
 	std::cin.get();
 	return 0;
-}
-
-template <typename T>
-void SimplePoolTestUnthreaded(T& allocator, std::fstream& p_file)
-{
-	Timer timer;
-	std::vector<Particle*> ptrs;
-	int frameCount = 0;
-	double totalTime = 0.0;
-
-	ptrs.reserve(SIMPLE_POOL_PARTICLE_COUNT);
-
-	for (size_t i = 0; i < SIMPLE_POOL_TEST_FRAME_COUNT; ++i) 
-	{
-		// Start timing
-		timer.Start();
-
-		for (size_t k = 0; k < SIMPLE_POOL_PARTICLE_COUNT; ++k)
-		{
-			ptrs.push_back(new (allocator.Alloc()) Particle(k));
-		}
-
-		for (size_t k = 0; k < SIMPLE_POOL_PARTICLE_COUNT; ++k)
-		{
-			allocator.Free(ptrs[k]);
-		}
-
-		ptrs.clear();
-
-		// Measure time.
-		double elapsed = timer.Stop();
-
-		// Store profiling data.
-		p_file << elapsed << std::endl;
-		totalTime += elapsed;
-		frameCount++;
-	}
-
-	std::cout << frameCount << " : " << totalTime / frameCount << std::endl;
 }
 
 /*
